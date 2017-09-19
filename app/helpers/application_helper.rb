377aa431c7e5390851019571591ab46f6b014457
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-09-19 13:28:49 -0700

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do #name of content block is ":title" and block will be yield in application.html.erb title
+        (parts.unshift("Wikiiki")).join(" - ")
+      end
+    end
+  end
 end
