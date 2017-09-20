16cd7e31a255a9bee161a1bfe2d1566c3fbfd0ed
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-09-20 14:10:05 -0700

    Add validation to ensure title are specified

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,37 @@ class PostsController < ApplicationController
   def index
   end
 
-  def new
+  def new #create an object
     @post = Post.new
   end
 
-  def create
+  #render vs redirect_to
+  #-----------------------
+  #render will render a particular view using the instance variables available in the action.
+  #For example if a render was used for the new action, when a user goes to /new, the new action
+  #in the controller is called, instance variables are created and then passed to the new view.
+  #Rails creates the html for that view and returns it back to the user’s browser. This is what you
+  # would consider a normal page load.
+  #
+  #redirect_to will send a redirect to the user’s browser telling it to re-request a new URL.
+  #Then the browser will send a new request to that URL and it will go through the action for that URL,
+  #oblivious to the fact that it was redirected to. None of the variables created in the action that caused
+  #the redirect will be available to the redirected view. This is what happens when you click on ‘Create’ in
+  #a form and the object is created and you’re redirected to the edit view for that object.
+
+  #flash vs flash_now
+  #------------------
+  #flash messages display on the very next page load
+  #flash.now flash messages display on the current page
+
+  def create #create an object & database record
     @post = Post.new(post_params)
     if @post.save
       flash[:notice] = "Post has been created."
-      redirect_to @post
+      redirect_to @post  #rails knows to goto post show page. You can also specify "redirect_to :action => :show"
     else
-    # nothing, yet
+      flash.now[:alert] = "Post has not been created."
+      render "new"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  root "posts#index" # defines a route for "/" to point at the index action of the PostsController
   resources :posts
 end
