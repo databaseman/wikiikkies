7afcaceee541a10b5fef1c0ef30845d2caf71338
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-10-18 11:44:12 -0700

    namespace 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'application#index'
+  end
+
   devise_for :users
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 
