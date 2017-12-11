02b064ac8e4c28e85e15803fabc001b22f8f5974
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-12-11 15:12:20 -0800

    rspec sign in feature

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -1,19 +1,21 @@
 require "rails_helper"
 RSpec.feature "Users sign in" do
-  let!(:user) { FactoryGirl.create(:user) }
-  scenario "with valid credentials" do
+
+  before do
     visit "/"
     click_link "Sign in"
+  end
+
+  let!(:user) { FactoryGirl.create(:user) }
+  scenario "with valid credentials" do
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Sign in"
     expect(page).to have_content "Signed in successfully."
-    expect(page).to have_content "Signed in as #{user.email}"
+    expect(page).to have_content "#{user.name} (#{user.email})"
   end
 
   scenario "when providing invalid email" do
-    visit "/"
-    click_link "Sign in"
     fill_in "Email", with: "test@.com"
     fill_in "user_password", with: user.password
     click_button "Sign in"
@@ -21,8 +23,6 @@ RSpec.feature "Users sign in" do
   end
 
   scenario "when providing invalid password" do
-    visit "/"
-    click_link "Sign in"
     fill_in "Email", with: user.email
     fill_in "user_password", with: "somepassword"
     click_button "Sign in"
