dcebf7878c8f8a37507b8be4bd1f3ec95c5580d1
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2018-08-19 14:21:55 -0700

    remove attachment from posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-  mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
   belongs_to :user
 
   has_many   :collaborators, dependent: :destroy
