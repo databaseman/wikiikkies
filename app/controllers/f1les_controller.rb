a8e6aa0c0ee3c045cc6afcf078d39eaf7acf13b5
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2018-10-28 12:06:35 -0700

    added cancel to attachment

--- app/controllers/f1les_controller.rb
+++ app/controllers/f1les_controller.rb
@@ -1,4 +1,6 @@
 class F1lesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
      @post=policy_scope(Post).find(params[:post_id])
      authorize @post, :update?
@@ -12,11 +14,14 @@ class F1lesController < ApplicationController
   def create
      @f1le = F1le.new(f1le_params)
      @f1le.post_id=params[:post_id]
-     if @f1le.save
-        redirect_to post_f1les_path, notice: "The file #{@f1le.name} has been uploaded."
+     if @f1le.save # Calling database save/insert command
+       flash[:success] = 'File was added.'
+       redirect_to post_f1les_path
      else
-        render "new"
+       flash.now[:danger] = 'There was an error adding file . Please try again.'
+       redirect_to action: "index", id: @f1le.post_id # render the new view again
      end
+
   end
 
   def destroy
